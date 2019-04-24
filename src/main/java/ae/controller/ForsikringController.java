85a9a795c0f0997db086a03f107780a0155e43ac
Hans Erling Eidsvold <hans@norcule.com>
2019-04-24 12:34:35 +0200

    Forsikring + Båtforsikring implementert. Controller påbegynt.

--- src/main/java/ae/controller/ForsikringController.java
+++ src/main/java/ae/controller/ForsikringController.java
@@ -1,8 +1,32 @@
 package ae.controller;
 
+import ae.model.Forsikring;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.time.LocalDate;
 
 public class ForsikringController {
+
+    // tabellen
+    @FXML
+    public TableView<Forsikring> forsikringTabell;
+    @FXML
+    public TableColumn<Forsikring, Number> kundeKolonne;
+    @FXML
+    public TableColumn<Forsikring, Number> forsikringsnrKolonne;
+    @FXML
+    public TableColumn<Forsikring, LocalDate> datoOpprettetKolonne;
+    @FXML
+    public TableColumn<Forsikring, Number> forsikringsbelopKolonne;
+    @FXML
+    public TableColumn<Forsikring, String> betingelserKolonne;
+    @FXML
+    public TableColumn<Forsikring, String> typeKolonne;
+
+    @FXML
     public void gåTilNyBåtforsikringPopup(ActionEvent actionEvent) {
     }
 }
--- src/main/java/ae/model/Båtforsikring.java
+++ src/main/java/ae/model/Båtforsikring.java
@@ -16,19 +16,78 @@ public class Båtforsikring extends Forsikring {
 
     // default konstruktør
     public Båtforsikring(Kunde kunde, int forsikringsNr) {
-        this(kunde, forsikringsNr, LocalDate.now(), 0, null, null,
+        this(kunde, forsikringsNr, LocalDate.now(), 0, null, null, null,
                 null, 0, 0, null);
     }
 
     // non-default konstruktør
     public Båtforsikring(Kunde kunde, int forsikringsNr, LocalDate datoOpprettet, int forsikringsBelop,
-                         String betingelser, String registreringsNr,  String typeModell, int lengdeFot,
+                         String betingelser, String type, String registreringsNr,  String typeModell, int lengdeFot,
                          int årsmodell, String motorEgenskaper) {
-        super(kunde, forsikringsNr, datoOpprettet, forsikringsBelop, betingelser);
+        super(kunde, forsikringsNr, datoOpprettet, forsikringsBelop, betingelser, type);
         this.registreringsNr = new SimpleStringProperty(registreringsNr);
         this.typeModell = new SimpleStringProperty(typeModell);
         this.lengdeFot = new SimpleIntegerProperty(lengdeFot);
         this.årsmodell = new SimpleIntegerProperty(årsmodell);
         this.motorEgenskaper = new SimpleStringProperty(motorEgenskaper);
+    }
+
+    /**
+     * Get- og Set-metoder for datafeltene
+     */
+
+    // registreringsNr
+    public String getRegistreringsNr() {
+        return registreringsNr.get();
+    }
+    public void setRegistreringsNr(String registreringsNr) {
+        this.registreringsNr.set(registreringsNr);
+    }
+    public StringProperty registreringsNrProperty() {
+        return registreringsNr;
+    }
+
+    // typeModell
+    public String getTypeModell() {
+        return typeModell.get();
+    }
+    public void setTypeModell(String typeModell) {
+        this.typeModell.set(typeModell);
+    }
+    public StringProperty typeModellProperty() {
+        return typeModell;
+    }
+
+    // lengdeFot
+    public int getLengdeFot() {
+        return lengdeFot.get();
+    }
+    public void setLengdeFot(int lengdeFot) {
+        this.lengdeFot.set(lengdeFot);
+    }
+    public IntegerProperty lengdeFotProperty() {
+        return lengdeFot;
+    }
+
+    // årsmodell
+    public int getÅrsmodell() {
+        return årsmodell.get();
+    }
+    public void setÅrsmodell(int årsmodell) {
+        this.årsmodell.set(årsmodell);
+    }
+    public IntegerProperty årsmodellPropert() {
+        return årsmodell;
+    }
+
+    // motorEgenskaper
+    public String getMotorEgenskaper() {
+        return motorEgenskaper.get();
+    }
+    public void setMotorEgenskaper(String motorEgenskaper) {
+        this.motorEgenskaper.set(motorEgenskaper);
+    }
+    public StringProperty motorEgenskaperProperty() {
+        return motorEgenskaper;
     }
 }
